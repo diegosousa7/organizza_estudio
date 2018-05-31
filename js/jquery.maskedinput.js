475661fe215240aee5ccbaaba90a85b40467d0e5
danielaureliano <daniel.aureliano@gmail.com>
2018-05-31 00:39:09 -0300

    Correção da Tela de cadastro de cliente

---
+++ js/jquery.maskedinput.js
@@ -0,0 +1,12 @@
+/**
+ * Created by AURELIANO on 28/05/2018.
+ */
+jQuery.noConflict();
+jQuery(function ($) {
+    $("#niver").mask("99/99/9999");
+    $("#telefone").mask("(99) 9999-9999");
+    $("#celular").mask("(99) 99999-9999");
+    $("#cep").mask("99999-999");
+    $("#cnpj").mask("99.999.999/9999-99");
+    $("#inscricaoEstadual").mask("99.999.999/999-99");
+});
