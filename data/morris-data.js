67e892d57f114814cc9b7b6edce3e920228645dc
Daniel Aureliano <daniel.aureliano@gmail.com>
2018-06-10 18:24:39 -0300

    Revisado TCC Referência Bibliográfica e alteração de parte do texto. Adicionada função de confirmação antes de deletar itens e alterados dados gráfico da index-adm.

--- data/morris-data.js
+++ data/morris-data.js
@@ -3,59 +3,54 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2016 Q1',
+            "Pacote Fotografia de Casamento": 2666,
+            "Pacote Fotografia 15 Anos": null,
+            "Pacote Fotografia Formatura": 2647
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2016 Q2',
+            "Pacote Fotografia de Casamento": 2778,
+            "Pacote Fotografia 15 Anos": 2294,
+            "Pacote Fotografia Formatura": 2441
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2016 Q3',
+            "Pacote Fotografia de Casamento": 4912,
+            "Pacote Fotografia 15 Anos": 1969,
+            "Pacote Fotografia Formatura": 2501
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2016 Q4',
+            "Pacote Fotografia de Casamento": 3767,
+            "Pacote Fotografia 15 Anos": 3597,
+            "Pacote Fotografia Formatura": 5689
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2017 Q1',
+            "Pacote Fotografia de Casamento": 6810,
+            "Pacote Fotografia 15 Anos": 1914,
+            "Pacote Fotografia Formatura": 2293
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2017 Q2',
+            "Pacote Fotografia de Casamento": 5670,
+            "Pacote Fotografia 15 Anos": 4293,
+            "Pacote Fotografia Formatura": 1881
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2017 Q3',
+            "Pacote Fotografia de Casamento": 4820,
+            "Pacote Fotografia 15 Anos": 3795,
+            "Pacote Fotografia Formatura": 1588
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2017 Q4',
+            "Pacote Fotografia de Casamento": 15073,
+            "Pacote Fotografia 15 Anos": 5967,
+            "Pacote Fotografia Formatura": 5175
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2018 Q1',
+            "Pacote Fotografia de Casamento": 10687,
+            "Pacote Fotografia 15 Anos": 4460,
+            "Pacote Fotografia Formatura": 2028
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['Pacote Fotografia de Casamento', 'Pacote Fotografia 15 Anos', 'Pacote Fotografia Formatura'],
+        labels: ['Pacote Fotografia de Casamento', 'Pacote Fotografia 15 Anos ', 'Pacote Fotografia Formatura'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -95,15 +90,15 @@ $(function() {
             a: 75,
             b: 65
         }, {
-            y: '2010',
+            y: '2016',
             a: 50,
             b: 40
         }, {
-            y: '2011',
+            y: '2017',
             a: 75,
             b: 65
         }, {
-            y: '2012',
+            y: '2018',
             a: 100,
             b: 90
         }],
